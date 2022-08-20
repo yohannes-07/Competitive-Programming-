e94b6da504e4e451da27ae7d6200f70507da3942
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2022-08-20 06:16:03 +0300

    Time: 2058 ms (65.48%), Space: 28 MB (33.41%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -5,14 +5,16 @@ class Solution:
         
         left, right = 0, 0
         output, sum1 = 0, 0
-        #
+        
         while right < len(nums):
             sum1 += nums[right]
             
             while nums[right] * (right - left + 1) > sum1 + k:
                 sum1 -= nums[left]
                 left += 1
+                
             output = max(output, right-left + 1)
             right += 1
+            
         return output
         
