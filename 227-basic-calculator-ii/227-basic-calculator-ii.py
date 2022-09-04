69ce3f0996c7dde0d47fbe9967afd432efdce4ec
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2022-09-04 09:54:30 +0300

    Time: 198 ms (27.14%), Space: 22.4 MB (12.77%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -1,21 +1,38 @@
 class Solution(object):
     def calculate(self, s):
-        num, op, stack = 0, '+', [0]
-        ops = {'+':lambda x, y: y, '-':lambda x, y: -y, '*':lambda x, y: x*y, '/':lambda x, y: (int)(float(x)/float(y))}
-        for i, c in enumerate(s):
-            if c.isdigit():
-                num = num * 10 + int(c)
-            if not c.isdigit() and c != ' ' or i == len(s)-1:
-                prev = 0 if op in '+-' else stack.pop()
-                stack.append(ops[op](prev, num))
-                num, op = 0, c
-                
-        output = 0
-        
-        for j in range(len(stack)):
-             output += stack[j]
-                
-        return output 
-                       
-                       
-                       
+        if not s:
+            return "0"
+        else:
+            stack, num, sign = [], 0, "+"
+
+            for i in range(len(s)):
+
+                if s[i].isdigit():
+                    num = num*10+ord(s[i])-ord("0")
+
+                if (not s[i].isdigit() and not s[i].isspace()) or i == len(s)-1:
+                    if sign == "-":
+                        stack.append(-num)
+
+                    elif sign == "+":
+                        stack.append(num)
+
+                    elif sign == "*":
+                        stack.append(stack.pop()*num)
+
+                    else: 
+                        tmp = stack.pop()
+                        tmp = abs(tmp)//num if tmp >= 0 else -(abs(tmp)//num)
+                        stack.append(tmp)
+
+                    sign = s[i]
+                    num = 0
+
+            output = 0
+
+            for j in range(len(stack)):
+                 output += stack[j]
+
+            return output 
+
+
