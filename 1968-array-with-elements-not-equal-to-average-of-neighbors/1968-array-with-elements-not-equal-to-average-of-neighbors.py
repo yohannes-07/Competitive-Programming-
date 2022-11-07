1fdd619e7d1f0dce69e080a81073eae294a047bc
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2022-11-07 07:08:29 -0500

    Time: 3496 ms (37.77%), Space: 29.7 MB (91.85%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.py
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.py
@@ -1,19 +1,7 @@
 class Solution:
-    def rearrangeArray(self, nums: List[int]) -> List[int]:
-        nums1 = sorted(nums)
-        
-        output = [None]*len(nums)
-        
-        midx = len(nums)//2
-        mid= nums1[midx]
-        
-        for i in range(1, len(nums), 2):
-            if nums1[0] < mid:
-                last_elem = nums1.pop(0)
-                output[i] = last_elem
-                
-        for i in range(0, len(nums), 2):
-            if len(nums1):
-                last_elem = nums1.pop(0)
-                output[i] = last_elem
-        return output
+    def rearrangeArray(self, A: List[int]) -> List[int]:
+        A.sort()
+        for i in range(1, len(A), 2):
+            A[i], A[i - 1] = A[i - 1], A[i]
+        print(A)
+        return A
