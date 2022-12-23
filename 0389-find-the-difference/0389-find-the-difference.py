8b8622695e5680f055d856edd9757b738fd5e9e9
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2022-12-23 18:45:15 +0300

    Time: 43 ms (78.36%), Space: 13.9 MB (75.64%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -2,24 +2,32 @@ class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         if not s:
             return t
-        dic = {}
-        dic1 = {}
+        
+        # we use two dictionaries and store each strings' char as key and thier frequency 
+        # as vlaue like {"a": 1, "b": 3}
+        
+        dic1 = {} # for string s
+        dic2 = {} # for string t
         
         for char in s:
-            if char in dic:
-                dic[char] += 1    
+            if char in dic1:
+                dic1[char] += 1    
             else:
-                dic[char] = 1
+                dic1[char] = 1
         
         for char in t:
-            if char in dic1:
-                dic1[char] += 1
+            if char in dic2:
+                dic2[char] += 1
                 
             else:
-                dic1[char] = 1 
-        for key in dic1:
-            if key in dic and dic1[key] > dic[key]:
+                dic2[char] = 1 
+        
+        # check if one of the keys in dic2 is not in dic1
+        # or if the key frequency in dic2 is greater than in dic1
+        
+        for key in dic2:
+            if key in dic1 and dic2[key] > dic1[key]:
                 return key
-            if key not in dic:
+            if key not in dic1:
                 return key
       
