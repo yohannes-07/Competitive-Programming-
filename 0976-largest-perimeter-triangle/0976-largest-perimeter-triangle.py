8dbad7a3e4bdbfb696c51fc323d3fe1daa880782
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2022-12-26 22:25:28 +0300

    Time: 269 ms (69.75%), Space: 15.5 MB (8.39%) - LeetHub

---
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
@@ -0,0 +1,23 @@
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        
+        #sort the arrray from highest to lowest
+        nums.sort()
+        nums = nums[::-1]
+        n = len(nums) - 2
+        
+        res = 0
+        i = 0
+        while i < n:
+            
+            #if the first largest num is greater than the sum of consecutive nums it can form triangle
+            if nums[i + 1] + nums[i + 2] > nums[i]:
+                
+                #aki check if its area is different from 0
+                if nums[i + 1] * nums[i + 2] * nums[i]:
+                    perimeter = nums[i + 1] + nums[i + 2] + nums[i]
+                    res = max(res, perimeter)
+                
+            i += 1
+        return res
+            
