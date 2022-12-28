c00c3649b68cb7a33e985edd980b53fc527d8b04
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2022-12-28 10:33:30 +0300

    Time: 26 ms (98.92%), Space: 13.8 MB (56.49%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,9 +1,28 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        ctr = 0
-        output = []
-        for i in range(len(nums)):
-            for j in range(i, len(nums)):
-                 if nums[i] == nums[j] and i<j:
-                    ctr += 1
-        return ctr
+        
+        #similar to #2506 Count Pairs of similar strings
+        dic = {}
+        res = 0
+        
+        # put the frequency of each num in dic
+        for num in nums:
+            if num in dic:
+                dic[num] += 1
+                
+            else:
+                dic[num] = 1
+                
+        # iterate through the dic value and find no of pairs of similar strings
+        # use combination rule to find no of pairs => c(n, p) = n!/((n-p)! * p!)
+        #p is 2 in this problem ....the combination rule satisfies i < j rule
+
+        for value in dic.values():
+            if value == 1:
+                continue
+                
+            else:
+                comb = (math.factorial(value))/(math.factorial(value - 2) * 2)
+                res += int(comb)
+                
+        return res
