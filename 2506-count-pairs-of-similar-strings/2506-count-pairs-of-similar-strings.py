d119e4e02a835795710c1cd669093db1cc336cbb
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2022-12-28 10:08:12 +0300

    Time: 43 ms (100.00%), Space: 14.1 MB (39.11%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
@@ -3,21 +3,32 @@ class Solution:
     def similarPairs(self, words: List[str]) -> int:
         dic = {}
         for word in words:
-            word = set(word)
-            word = list(word)
+            
+            #convert eache word to set so that no duplicated letters
+            #change the set to list to sort elemnts and 
+            #change it to string to make ur key hashable(immutabel)
+            
+            word = list(set(word))
             word = sorted(word)
             word = "".join(word)
-            print(word)
+         
             if word in dic:
                 dic[word] += 1
+                
             else:
                  dic[word] = 1
-        print(dic)
+   
+        # iterate through the dic value and find no of pairs of similar strings
+        # use combination rule to find no of pairs => c(n, p) = n!/((n-p)! * p!)
+        #p is 2 in this problem
+        
         ans = 0
         for value in dic.values():
             if value == 1:
                 continue
+                
             else:
                 comb = (math.factorial(value))/(math.factorial(value - 2) * 2)
                 ans += int(comb)
+                
         return ans
