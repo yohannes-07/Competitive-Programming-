cb7ed55e4b2197b45d7cebe4f54d3a9ff1403045
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-01-13 17:58:14 +0300

    Time: 18 ms (94.27%), Space: 13.6 MB (38.22%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,10 +1,17 @@
 class Solution:
     def findTheWinner(self, n, k):
-        nums = list(range(1, n + 1))
-        print(nums)
-        i = 0
-        while len(nums) > 1: 
-            i = (i + k-1) % len(nums)
-            popped = nums.pop(i)
-            print(popped)
-        return nums[0] 
+        
+        #generate list of players
+        players = list(range(1, n + 1))
+        initial_position = 0
+        
+        # len of players decrease by on at a time when we pop a player
+        # continue unti one player is left
+        while len(players) > 1: 
+            
+            #starring from the initial position count k times and subtract 1 as 
+            #we should include the starting position and pop that player
+            initial_position = (initial_position + k-1) % len(players)
+            players.pop(initial_position)
+          
+        return players[0] 
