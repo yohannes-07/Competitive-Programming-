197ea4341442c65331c50d1671f3c96851dc77d1
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-01-25 18:25:42 +0300

    Time: 92 ms (14.75%), Space: 13.9 MB (15.22%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -3,16 +3,16 @@ class Solution:
         n = len(s)
         res = []
         dic = {}
-        left = right = 0
+        left = right_occurence_of_char = 0
         
         for i in range(n):
             dic[s[i]] = i
-        
+
         for j in range(n):
-            right = max(right, dic[s[j]])
+            right_occurence_of_char = max(right_occurence_of_char, dic[s[j]])
             
-            if j == right:
-                res.append(right - left + 1)
+            if j == right_occurence_of_char:
+                res.append(right_occurence_of_char - left + 1)
                 left = j + 1
                 
         return res
