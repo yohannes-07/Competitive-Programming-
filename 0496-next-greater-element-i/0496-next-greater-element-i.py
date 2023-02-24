f2dc990e50008d3219aecd319b32a0d9e45b0f1b
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-02-24 11:58:53 +0300

    Time: 47 ms (87.79%), Space: 14.1 MB (48.22%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,19 +1,19 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         res = []
-        dic = {}
-        mstack = []
+        next_greater = {}
+        min_stack = []
         
         for num in nums2: 
-            while mstack and mstack[-1] <= num:
-                popped = mstack.pop()
-                dic[popped] = num
+            while min_stack and min_stack[-1] <= num:
+                popped = min_stack.pop()
+                next_greater[popped] = num
             
-            mstack.append(num)
+            min_stack.append(num)
             
    
         for num in nums1:
-            res.append(dic.get(num, -1))
+            res.append(next_greater.get(num, -1))
                          
         return res
             
