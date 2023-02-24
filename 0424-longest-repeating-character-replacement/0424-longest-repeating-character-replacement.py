150aae070cb187afc5e7d3434f2597f940e2efb8
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-02-24 20:51:49 +0300

    Time: 88 ms (98.36%), Space: 13.9 MB (51.30%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,24 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = longest_substring = most_freq = 0
+        chars_freq = defaultdict(int)
+        
+        for i in range(len(s)):
+            chars_freq[s[i]] += 1
+            most_freq = max(most_freq, chars_freq[s[i]])
+            
+            # longest substring - most frequent char <= k if the formula is valid
+            # it means we get the longest substring with operations <= k
+            # as the most freq char makes longest substring
+            
+            #if invalid window, subtract the left most elem from it
+            if i - left + 1 - most_freq > k:
+                chars_freq[s[left]] -= 1
+                left += 1
+            
+            else:
+                
+                longest_substring = max(longest_substring,  i - left + 1)
+                
+                
+        return longest_substring
