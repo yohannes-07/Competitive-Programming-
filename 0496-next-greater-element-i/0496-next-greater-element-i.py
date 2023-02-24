706884ae74752143ecefd86956ee1c1549fca3cb
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-02-24 11:53:57 +0300

    Time: 41 ms (97.48%), Space: 14.1 MB (93.62%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,8 +1,9 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        res = [-1] * len(nums1)
+        res = []
         dic = {}
         mstack = []
+        
         for num in nums2: 
             while mstack and mstack[-1] <= num:
                 popped = mstack.pop()
@@ -11,8 +12,8 @@ class Solution:
             mstack.append(num)
             
    
-        for i, num in enumerate(nums1):
-            res[i] = dic.get(num, -1)
+        for num in nums1:
+            res.append(dic.get(num, -1))
                          
         return res
             
