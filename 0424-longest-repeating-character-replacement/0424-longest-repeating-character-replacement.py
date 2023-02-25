916be28fe56ba52f3f0d67c0fed0a697c268f290
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-02-25 22:20:09 +0300

    Time: 111 ms (88.29%), Space: 13.9 MB (51.34%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -9,16 +9,15 @@ class Solution:
             
             # longest substring - most frequent char <= k if the formula is valid
             # it means we get the longest substring with operations <= k
-            # as the most freq char makes longest substring
+            # as the most freq char makes the longest substring
             
             #if invalid window, subtract the left most elem from it
             if i - left + 1 - most_freq > k:
                 chars_freq[s[left]] -= 1
                 left += 1
             
-            else:
                 
-                longest_substring = max(longest_substring,  i - left + 1)
+            longest_substring = max(longest_substring,  i - left + 1)
                 
                 
         return longest_substring
