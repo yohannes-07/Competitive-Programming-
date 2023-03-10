4f2ff5180afd68b9c6bab2b6779000e2d4dea966
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-03-10 17:03:04 +0300

    Time: 35 ms (69.16%), Space: 14 MB (77.75%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        def backtrack(i, acc):
+        
+            if i >= len(nums):
+                res.append(acc[:])
+                return
+            
+            #descion to add the curr value
+            acc.append(nums[i])
+            backtrack(i + 1, acc)
+            acc.pop()
+            
+            # descion not to add the curr value
+            # so call the function by passing the next value as an argument
+            
+            backtrack(i + 1, acc)
+            
+            
+        backtrack(0, [])
+        
+        return res
