05387ff9632587415a85c439a799614e49a22dce
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-03-10 16:59:34 +0300

    Time: 423 ms (51.70%), Space: 16 MB (43.77%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -2,21 +2,26 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
         
-        def backtrack(start, acc):
+        def backtrack(i, acc):
+            
             if len(acc) == k:
                 res.append(acc[:])
                 return
             
-            for i in range(start, n + 1):
-                #placing
-                acc.append(i)
-                
-                #recursive call
-                backtrack(i + 1, acc)
-                
-                #remove
-                acc.pop()
-                
+            if i > n:
+                return
+            
+            #descion to add the curr value
+            acc.append(i)
+            backtrack(i + 1, acc)
+            acc.pop()
+            
+            # descion not to add the curr value
+            # so call the function by passing the next value as an argument
+            
+            backtrack(i + 1, acc)
+            
+            
         backtrack(1, [])
         
         return res
