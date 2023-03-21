eb7860cefeeb0dfbc2d22c8b4092ac509d90c34f
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-03-21 19:52:17 +0300

    Time: 134 ms (39.44%), Space: 14 MB (57.62%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -1,24 +1,41 @@
 class Solution:
     def maxLength(self, arr: List[str]) -> int:
+        
+        def uniqueChars(arr):
+            uniques = []
+            
+            for string in arr:
+                if len(set(string)) == len(string):
+                    uniques.append(string)
+                    
+            return uniques
+        
+        
+        arr = uniqueChars(arr)
         res = 0
- 
+        seen = set()
+        
         def backtrack(start, acc):
-            nonlocal res
+            nonlocal res, seen
             
             if start > len(arr):
                 return
             
-            temp = "".join(acc)  
-            if temp and max(Counter(temp).values()) == 1:
-                
-                res = max(res, len("".join(acc)))
-             
+            res = max(res, len(seen))
+            
             for i in range(start, len(arr)):
+                
+                if len(seen.intersection(arr[i])) > 0:
+                    continue
+                    
                 acc.append(arr[i])
+                seen = seen.union(arr[i])
                 
                 backtrack(i + 1, acc)
                 
-                acc.pop()
+                temp = acc.pop()
+                
+                seen = seen - set(temp)
                 
                 
         backtrack(0, [])
