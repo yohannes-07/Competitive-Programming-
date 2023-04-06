6f58803075e4384904950d6d520ef8316a851cfe
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-04-06 19:56:02 +0300

    Time: 801 ms (79.36%), Space: 49.9 MB (60.57%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -1,15 +1,19 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
         n = len(edges)
-        freq = [0] * (n + 1)
+        freq = defaultdict(int)
         
         for u, v in edges:
             
+            if u in freq:
+                return u
+            
+            if v in freq:
+                return v
        
-            freq[u - 1] += 1
-            freq[v - 1] += 1
+            freq[u ] += 1
+            freq[v ] += 1
             
-        max_ = max(freq)
+            
+            
         
-        return freq.index(max_) + 1
-            
