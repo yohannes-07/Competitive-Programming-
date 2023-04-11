7cce7564b7742bc721404ffe5b799f63845f94cf
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-04-11 19:14:59 +0300

    Time: 706 ms (67.02%), Space: 22.5 MB (12.64%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -0,0 +1,39 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        
+        graph = defaultdict(list)
+        
+
+        for a, b in dislikes:
+            graph[a].append(b)
+            graph[b].append(a)
+    
+        visited = set()
+        color = [0 for _ in range(n + 1)]
+        
+        def dfs(node):
+            
+            visited.add(node)
+           
+            
+            for neigbhour in graph[node]:
+                if neigbhour not in visited and  not color[neigbhour]:
+                    visited.add(neigbhour)
+                    
+                    color[neigbhour] = -1 * color[node]
+                    
+                    if not dfs(neigbhour): return False
+                    
+                elif color[node] == color[neigbhour]:
+                    return False
+                
+            return True
+        
+        
+        for node in graph:
+        
+            if node not in visited:
+                color[node] = 1
+                if not dfs(node): return False
+                
+        return True
