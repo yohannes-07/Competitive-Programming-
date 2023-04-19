274145ed2e1fa4ebc15a2a6f859751e62e504ed5
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-04-19 23:30:31 +0300

    Time: 147 ms (31.71%), Space: 15.9 MB (64.05%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,37 @@
+class Solution:
+    def solve(self, grid: List[List[str]]) -> None:
+     
+        m, n = len(grid), len(grid[0])
+        
+        def inBound(row, col):
+            return 0 <= row < m and 0 <= col < n
+        
+        
+        def dfs(row, col):
+            if not inBound(row, col) or grid[row][col] != "O":
+                return 
+
+            grid[row][col] = "temp"
+
+            dfs(row + 1, col)
+            dfs(row - 1, col)
+            dfs(row, col + 1)
+            dfs(row, col - 1)
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "O" and (i == 0 or i == m - 1 or j == 0 or j == n - 1):
+                    
+                    dfs(i, j)
+            
+        for i in range(m):
+            for j in range(n): 
+                if grid[i][j] == "O":
+                  
+                    grid[i][j] = "X"
+                 
+                  
+        for i in range(m):
+            for j in range(n): 
+                if grid[i][j] == "temp":
+                    grid[i][j] = "O"
