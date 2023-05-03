bdf646b9f7c337f331ebba3fbf3410b1ea57a0fd
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-05-03 19:45:09 +0300

    Time: 532 ms (63.18%), Space: 27.5 MB (35.68%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,36 +1,11 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq._heapify_max(nums)
         
-        def quickSort(start, end, nums, k):
-            if start >= end:
-                return 
+        for _ in range(k - 1):
             
-            pivot = nums[start]
-            l = start + 1
-            
-            for r in range(start + 1, end + 1):
-                if nums[r] <= pivot:
-                    nums[r], nums[l] = nums[l], nums[r]
-                    l += 1
-                    
-            nums[start], nums[l-1] = nums[l -1], nums[start]
-            
-            if k <= len(nums) - l:
-                quickSort(l, end, nums, k)
-                    
-            else:
-                quickSort(start, l - 2, nums, k)
-                
-        quickSort(0, len(nums) - 1, nums, k)
+            heapq._heappop_max(nums)
         
-        print(nums)
-        return nums[-k]
-            
+        return nums[0]
         
-                    
-                
-                    
-                    
-                
-        
-        
+      
