8b0bd27d7d7b01b4c5fd1593018ad0e7860a87c6
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-05-23 21:34:10 +0300

    Time: 42 ms (76.68%), Space: 16.3 MB (88.47%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,7 +1,7 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         rep = {}
-        size = defaultdict(lambda:1)
+        value = defaultdict(lambda:1)
         
         def  find(x):
             if x not in rep:
@@ -18,24 +18,24 @@ class Solution:
             xrep = find(x)
             yrep = find(y)
             
-            ratio = (size[y] * q)/ size[x]
+            ratio = (value[y] * q)/ value[x]
             
             for node, par in rep.items():
                 if par == xrep:
                     rep[node] = yrep
-                    size[node] *= ratio
+                    value[node] *= ratio
                     
         
-        for (x, y), value in zip(equations, values):
-            union(x, y, value)
+        for (x, y), val in zip(equations, values):
+            union(x, y, val)
             
         res = []   
         for x, y in queries:
-            if x not in size or y not in size or find(x) != find(y):
+            if x not in value or y not in value or find(x) != find(y):
                 res.append(-1.0)
                 
             else:
-                res.append(size[x] / size[y])
+                res.append(value[x] / value[y])
             
                     
         return res
