5692d333d65a273b530eaf1db4cf7a5733ce8e26
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-05-26 18:48:43 +0300

    Time: 44 ms (31.49%), Space: 16.3 MB (28.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,9 +3,10 @@ class Solution:
         memo = {1: 1, 2: 2}
         
         def dp(n):
-            if n not in memo:
-                memo[n] = dp(n-1) + dp(n-2)
-                
+            if n in memo:
+                return memo[n]
+            
+            memo[n] = dp(n-1) + dp(n-2)
             return memo[n]
-        
+            
         return dp(n)
