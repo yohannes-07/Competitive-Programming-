c17cbdee6e5c3378158b6af44c576aa8c71edfd3
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-05-29 01:15:43 +0300

    Time: 865 ms (19.40%), Space: 29.2 MB (5.01%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -1,11 +1,14 @@
 class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
-        ans = nums[0]
-        sum_ = nums[0]
-        for i in range(1,len(nums)):
-            if nums[i] > sum_/i:
-                sum_ += nums[i]
-                ans = max(ans,ceil(sum_/(i+1)))
-            else:
-                sum_ += nums[i]
-        return ans
+        res = summ = nums[0]
+        
+        for i in range(1, len(nums)):
+            best = ceil(summ /  i)
+            summ += nums[i]
+            
+            j = i + 1
+            curr = ceil(summ / j)
+            res = max(res, best, curr)
+            
+                
+        return res
