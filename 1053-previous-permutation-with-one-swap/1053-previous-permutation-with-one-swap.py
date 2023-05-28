7998551be79cba1c9b017cc2efcb77aa6e1768d5
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-05-29 00:19:10 +0300

    Time: 257 ms (7.88%), Space: 17.6 MB (52.70%) - LeetHub

---
+++ 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.py
@@ -0,0 +1,18 @@
+class Solution:
+    def prevPermOpt1(self, arr: List[int]) -> List[int]:
+        if arr == sorted(arr): return arr
+        
+        n = len(arr)
+        
+        def swap(i, n):
+            for j in range(n-1, i, -1):
+                
+                if arr[j] < arr[i] and  arr[j] != arr[j-1]:
+                    arr[i], arr[j] = arr[j], arr[i]
+                    
+                    return True
+                
+        for i in range(n-2, -1, -1):
+            if arr[i] > arr[i + 1]:
+                if swap(i, n): return arr
+            
