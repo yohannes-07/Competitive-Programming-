6afd6ac3e04c2cce8c655f52e02075988c121b58
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-07-13 19:06:27 +0300

    Time: 206 ms (11.57%), Space: 16.6 MB (9.02%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.py
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.py
@@ -1,32 +1,32 @@
 class Solution:
     def recurse(self, index, state):
-            if index == len(self.nums):
-                return 0
-            
-            if index in self.memo:
-                return self.memo[index]
-            
-            ans = 1
-            
-            for i in range(index + 1, len(self.nums)):
-                if state == 'positive' and self.nums[i] - self.nums[index] < 0:
-                    ans = max(ans, 1 + self.recurse(i, 'negative'))
-                    
-                elif state == 'negative' and self.nums[i] - self.nums[index] > 0:
-                    ans = max(ans, 1 + self.recurse(i, 'positive'))
-            
-            self.memo[index] = ans
+        if index == len(self.nums):
+            return 0
+
+        if index in self.memo:
             return self.memo[index]
-        
-    def wiggleMaxLength(self, nums: List[int]) -> int:
-        self.memo, res = {}, 1
-        self.nums = nums
-        
-        for i in range(1, len(nums)):
-            if self.nums[i] - self.nums[i - 1] > 0:
-                res = max(res, 1 + self.recurse(i, 'positive'))
+
+        ans = 1
+
+        for i in range(index + 1, len(self.nums)):
+            if state == 'positive' and self.nums[i] < self.nums[index]:
+                ans = max(ans, 1 + self.recurse(i, 'negative'))
                 
-            elif self.nums[i] - self.nums[i - 1] < 0:
+            elif state == 'negative' and self.nums[i] > self.nums[index]:
+                ans = max(ans, 1 + self.recurse(i, 'positive'))
+
+        self.memo[index] = ans
+        return self.memo[index]
+
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        self.memo = {}
+        self.nums = nums
+        res = 1
+
+        for i in range(1, len(nums)):
+            if self.nums[i] > self.nums[i - 1]:
+                res = max(res, 1 + self.recurse(i, 'positive'))
+            elif self.nums[i] < self.nums[i - 1]:
                 res = max(res, 1 + self.recurse(i, 'negative'))
-       
+
         return res
