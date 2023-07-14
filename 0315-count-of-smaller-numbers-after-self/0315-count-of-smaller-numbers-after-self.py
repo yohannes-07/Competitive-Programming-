f7a7f9c5231e977fb4d55afef02beb0558322df4
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-07-14 12:07:03 +0300

    Time: 3154 ms (54.71%), Space: 46.2 MB (21.46%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -1,44 +1,47 @@
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        arr=[] 
-        res=[0]*len(nums)
-        
-       
-        for i,num in enumerate(nums):
+        arr = []
+        res = [0] * len(nums)
+
+        for i, num in enumerate(nums):
             arr.append((num, i))
-    
+
         def merge(left, right):
-            l=0
-            r=0
-            temp=[]
-            numElemsRightArrayLessThanLeftArray=0
+            l = 0
+            r = 0
+            temp = []
+            countRightLessThanLeft = 0
+
             while l < len(left) and r < len(right):
                 if left[l][0] > right[r][0]:
                     temp.append(right[r])
                     r += 1
-                    numElemsRightArrayLessThanLeftArray += 1                 
+                    countRightLessThanLeft += 1
                 else:
                     temp.append(left[l])
-                    res[left[l][1]] += numElemsRightArrayLessThanLeftArray
+                    res[left[l][1]] += countRightLessThanLeft
                     l += 1
-            if l < (len(left)):
+
+            if l < len(left):
                 for i in range(l, len(left)):
                     temp.append(left[i])
-                    res[left[i][1]] += numElemsRightArrayLessThanLeftArray
-            if r < (len(right)):
-                for i in range(r,len(right)):
+                    res[left[i][1]] += countRightLessThanLeft
+
+            if r < len(right):
+                for i in range(r, len(right)):
                     temp.append(right[i])
+
             return temp
 
         def merge_sort(arr):
-            if len(arr)==1:
+            if len(arr) == 1:
                 return arr
-            
-            midIndex=len(arr)//2
-            left_side=merge_sort(arr[:midIndex])
-            right_side=merge_sort(arr[midIndex:])
-            
+
+            midIndex = len(arr) // 2
+            left_side = merge_sort(arr[:midIndex])
+            right_side = merge_sort(arr[midIndex:])
+
             return merge(left_side, right_side)
-        
+
         merge_sort(arr)
         return res
