68ac98ef62133db42c639247d80896a74f1a0029
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-07-14 12:06:13 +0300

    Time: 3195 ms (53.34%), Space: 46.4 MB (18.73%) - LeetHub

---
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -0,0 +1,44 @@
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        arr=[] 
+        res=[0]*len(nums)
+        
+       
+        for i,num in enumerate(nums):
+            arr.append((num, i))
+    
+        def merge(left, right):
+            l=0
+            r=0
+            temp=[]
+            numElemsRightArrayLessThanLeftArray=0
+            while l < len(left) and r < len(right):
+                if left[l][0] > right[r][0]:
+                    temp.append(right[r])
+                    r += 1
+                    numElemsRightArrayLessThanLeftArray += 1                 
+                else:
+                    temp.append(left[l])
+                    res[left[l][1]] += numElemsRightArrayLessThanLeftArray
+                    l += 1
+            if l < (len(left)):
+                for i in range(l, len(left)):
+                    temp.append(left[i])
+                    res[left[i][1]] += numElemsRightArrayLessThanLeftArray
+            if r < (len(right)):
+                for i in range(r,len(right)):
+                    temp.append(right[i])
+            return temp
+
+        def merge_sort(arr):
+            if len(arr)==1:
+                return arr
+            
+            midIndex=len(arr)//2
+            left_side=merge_sort(arr[:midIndex])
+            right_side=merge_sort(arr[midIndex:])
+            
+            return merge(left_side, right_side)
+        
+        merge_sort(arr)
+        return res
