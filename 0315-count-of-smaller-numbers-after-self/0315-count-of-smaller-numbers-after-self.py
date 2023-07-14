68fdb3a659a0e38ca651605db323f17cb550079d
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-07-14 12:08:37 +0300

    Time: 3285 ms (51.36%), Space: 46.2 MB (19.73%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -1,47 +1,42 @@
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        arr = []
+        arr = [(num, i) for i, num in enumerate(nums)]
         res = [0] * len(nums)
 
-        for i, num in enumerate(nums):
-            arr.append((num, i))
-
-        def merge(left, right):
-            l = 0
-            r = 0
-            temp = []
-            countRightLessThanLeft = 0
-
-            while l < len(left) and r < len(right):
-                if left[l][0] > right[r][0]:
-                    temp.append(right[r])
-                    r += 1
-                    countRightLessThanLeft += 1
-                else:
-                    temp.append(left[l])
-                    res[left[l][1]] += countRightLessThanLeft
-                    l += 1
-
-            if l < len(left):
-                for i in range(l, len(left)):
-                    temp.append(left[i])
-                    res[left[i][1]] += countRightLessThanLeft
-
-            if r < len(right):
-                for i in range(r, len(right)):
-                    temp.append(right[i])
-
-            return temp
-
         def merge_sort(arr):
-            if len(arr) == 1:
+            if len(arr) <= 1:
                 return arr
 
-            midIndex = len(arr) // 2
-            left_side = merge_sort(arr[:midIndex])
-            right_side = merge_sort(arr[midIndex:])
+            mid = len(arr) // 2
+            left = merge_sort(arr[:mid])
+            right = merge_sort(arr[mid:])
+            return merge(left, right)
 
-            return merge(left_side, right_side)
+        def merge(left, right):
+            merged = []
+            i, j = 0, 0
+            count_right_less_than_left = 0
+
+            while i < len(left) and j < len(right):
+                if left[i][0] > right[j][0]:
+                    merged.append(right[j])
+                    count_right_less_than_left += 1
+                    j += 1
+                else:
+                    merged.append(left[i])
+                    res[left[i][1]] += count_right_less_than_left
+                    i += 1
+
+            while i < len(left):
+                merged.append(left[i])
+                res[left[i][1]] += count_right_less_than_left
+                i += 1
+
+            while j < len(right):
+                merged.append(right[j])
+                j += 1
+
+            return merged
 
         merge_sort(arr)
         return res
