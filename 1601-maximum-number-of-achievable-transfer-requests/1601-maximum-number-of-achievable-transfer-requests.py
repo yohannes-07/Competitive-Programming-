0abd31518de0f82f66435f9f4225f49f08c212e0
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-07-15 16:22:53 +0300

    Time: 1669 ms (31.76%), Space: 16.3 MB (86.59%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
@@ -2,24 +2,24 @@ class Solution:
     def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
         transfers = [0] * n
         
-        def backtrack(curr, count):
+        def backtrack(i, count):
             
-            if curr >= len(requests):
+            if i >= len(requests):
                 
                 if all(t == 0 for t in transfers):
                     return count
                 
                 return 0
                 
-            transfers[requests[curr][0]] -= 1
-            transfers[requests[curr][1]] += 1
+            transfers[requests[i][0]] -= 1
+            transfers[requests[i][1]] += 1
             
-            count_included = backtrack(curr + 1, count + 1)
+            count_included = backtrack(i + 1, count + 1)
 
-            transfers[requests[curr][0]] += 1
-            transfers[requests[curr][1]] -= 1
+            transfers[requests[i][0]] += 1
+            transfers[requests[i][1]] -= 1
 
-            count_excluded = backtrack(curr + 1, count)
+            count_excluded = backtrack(i + 1, count)
 
             return max(count_included, count_excluded)
         
