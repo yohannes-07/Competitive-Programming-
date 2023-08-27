944e9560cdc5d97c1951c31e438912c2e84ed52d
Yohannes Ahunm <106800042+yohannes-07@users.noreply.github.com>
2023-08-27 11:01:13 +0300

    Time: 32 ms (95.23%), Space: 16.4 MB (47.71%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,8 +1,13 @@
 class Solution:
     def uniquePaths(self, m: int, n: int, memo={}) -> int:
         key = (m, n)
+        reversed_key = (n, m)
         
-        if key in memo: return memo[key]
+        # Both key or reversed_key return the same value
+        if (key) in memo: return memo[key]
+        if reversed_key in memo: return memo[reversed_key]
+        
+        # Base cases
         if m == 1 and n == 1: return 1
         if m == 0 or n == 0: return 0
         
