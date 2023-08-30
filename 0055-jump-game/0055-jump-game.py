8c4e240b8c49c27810895125c946909738c46933
Yohannes Ahunm <106800042+yohannes-07@users.noreply.github.com>
2023-08-30 14:17:22 +0300

    Time: 420 ms (68.12%), Space: 17.6 MB (60.94%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -6,11 +6,14 @@ class Solution:
         dp[0] = nums[0]
         
         for i in range(1, n - 1):
+            #Max of previous step and max distance you can reach starting from i(i + nums[i]) 
             dp[i] = max(i + nums[i], dp[i - 1])
             
+            #If you can't reach at i starting from i - 1
             if dp[i - 1] < i:
                 return False
             
             if dp[i] >= n - 1:
                 return True
+            
         return dp[n - 2] >= n - 1
