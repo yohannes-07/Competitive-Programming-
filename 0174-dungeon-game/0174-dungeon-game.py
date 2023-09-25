ee3242cd1cea61a3b77f4b00d7872e0bf04938c9
Yohannes Ahunm <106800042+yohannes-07@users.noreply.github.com>
2023-09-25 18:53:26 +0300

    Time: 74 ms (53.94%), Space: 17.4 MB (87.81%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.py
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -6,6 +6,7 @@ class Solution:
         # start = the min required value to enter to a cell
         # final = the min value after knight leaves a cell
         # start = final - delta
+        # start & final >= 1
         
         dp = [[0] * n for i in range(m)] 
         dp[m-1][n-1] = dungeon[m-1][n-1] * -1 + 1 if dungeon[m-1][n-1] < 0 else 1
@@ -33,6 +34,6 @@ class Solution:
                     start = max(1, final - delta)
                     dp[row][col] = start
                     
-        return dp[row][col]
+        return dp[0][0]
                     
         
