a8294b643ccff0321c0a74d69ae664a0c84d5188
Yohannes Ahunm <106800042+yohannes-07@users.noreply.github.com>
2023-10-03 18:11:43 +0300

    Time: 1657 ms (67.92%), Space: 26.1 MB (44.25%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
@@ -1,38 +1,31 @@
 class Solution:
     def maxScore(self, nums: List[int]) -> int:
         n = len(nums)
+        isVisitedAll = (1 << n)- 1
         
-        gcd_matrix = [[0] * n for _ in range(n)]
-        
-        for i in range(n):
-            for j in range(i+1, n):
-                gcd_matrix[i][j] = gcd_matrix[j][i] = gcd(nums[i], nums[j])
-        
-      
-        # initialize dp with length  2**n
-        dp = [0] * (1 << n)
-        
-        for state in range(1, 1 << n):
-          
-            cnt = bin(state).count('1')
-            #the number of  selected elements should be even as they are pair 
-            if cnt % 2 == 1:
-                continue
+        @cache
+        def backtrack(i, mask):
             
+            if mask == isVisitedAll:
+                return 0
             
-            for i in range(n):
-                # check if the curr elem is selected
-                if not (state & (1 << i)):
+            res = 0
+            for j in range(n-1):
+                if mask & (1 << j):
                     continue
-                for j in range(i+1, n):
-                    # check if the curr elem is selected
-                    if not (state & (1 << j)):
+                    
+                mask |= (1 << j)
+                
+                for k in range(j + 1, n):
+                    if mask & (1 << k):
                         continue
                     
-                    # Turn off i and j using XOR
-                    nextState = state ^ (1 << i) ^ (1 << j)
+                    mask |= (1 << k)
+                    res = max(res, i * gcd(nums[j], nums[k]) + backtrack(i + 1, mask))
                     
-                    # update maxScore for the curr state
-                    dp[state] = max(dp[state], dp[nextState] + cnt // 2 * gcd_matrix[i][j])
-        
-        return dp[(1 << n) - 1]
+                    mask = mask & ~(1 << k)
+                mask = mask & ~(1 << j)
+                    
+            return res
+            
+        return backtrack(1, 0)
