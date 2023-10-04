af2aec8742e10000e34d5516bef13b28b1c6c527
Yohannes Ahunm <106800042+yohannes-07@users.noreply.github.com>
2023-10-04 12:00:19 +0300

    Time: 1220 ms (22.30%), Space: 20.1 MB (60.02%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,35 +1,65 @@
 class Solution:
-    def checkIfPrerequisite(self, n: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
         
-        graph = defaultdict(list)
-        incoming = defaultdict(int)
-        precourses = defaultdict(set)
+#         Topological Sort
+         
+#         graph = defaultdict(list)
+#         incoming = defaultdict(int)
+#         precourses = defaultdict(set)
+        
+#         for pre, course in prerequisites:
+#             graph[pre].append(course)
+#             precourses[course].add(pre)
+#             incoming[course] += 1
+        
+#         queue = deque()
+#         for node in range(n):
+#             if incoming[node] == 0:
+#                 queue.append(node)
+                
+#         while queue:
+#             pre = queue.popleft()
+            
+#             for course in graph[pre]:
+#                 precourses[course] |= precourses[pre]
+                
+#                 incoming[course] -= 1
+                
+#                 if incoming[course] == 0:
+#                     queue.append(course)
+            
+#         ans = []
+#         for pre, course in queries:
+#             ans.append(pre in precourses[course])
+        
+#         return ans
+        
+#         Floyed Marshall
+
+        isPrerequisite = [[False] * numCourses for _ in range(numCourses)]
+        
         for pre, course in prerequisites:
-            graph[pre].append(course)
-            precourses[course].add(pre)
-            incoming[course] += 1
-             
+            isPrerequisite[pre][course] = True
+            
+            
+        for k in range(numCourses):
+            for i in range(numCourses):
+                for j in range(numCourses):
+                    
+                    if not isPrerequisite[i][j]  and (isPrerequisite[i][k] and isPrerequisite[k][j]):
+                        isPrerequisite[i][j] = True
+                        
+                        
+        res = []
         
-        queue = deque()
-        for node in range(n):
-            if incoming[node] == 0:
-                queue.append(node)
-                
-        while queue:
-            pre = queue.popleft()
-            
-            for course in graph[pre]:
-                precourses[course] |= precourses[pre]
-                
-                incoming[course] -= 1
-                
-                if incoming[course] == 0:
-                    queue.append(course)
-            
-        ans = []
-        print(precourses)
         for pre, course in queries:
-            ans.append(pre in precourses[course])
-        
-        return ans
+            res.append(isPrerequisite[pre][course])
+            
+        return res
+    
+    
+    
+    
+    
+    
         
